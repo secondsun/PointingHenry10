843f12ea650d4d52136663ebd34bb878673b668e
Corinne Krych <corinnekrych@gmail.com>
2016-10-09 10:21:31 +0100

    Correct List.xaml

--- PointingHenry10/Views/List.xaml.cs
+++ PointingHenry10/Views/List.xaml.cs
@@ -7,10 +7,7 @@ using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
+using PointingHenry10.ViewModels;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
