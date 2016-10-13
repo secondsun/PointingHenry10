7fe91670d50d2a1ac813baed843936e541cd4f44
Erik Jan de Wit <erikjan.dewit@gmail.com>
2016-10-13 16:58:34 +0200

    moved user input box to main page

--- PointingHenry10/ViewModels/MainPageViewModel.cs
+++ PointingHenry10/ViewModels/MainPageViewModel.cs
@@ -3,55 +3,58 @@ using System.Collections.Generic;
 using System;
 using System.Linq;
 using System.Threading.Tasks;
-using Template10.Services.NavigationService;
+using Windows.UI.Popups;
 using Windows.UI.Xaml.Navigation;
+using PointingHenry10.Models;
 
 namespace PointingHenry10.ViewModels
 {
     public class MainPageViewModel : ViewModelBase
     {
-        public MainPageViewModel()
-        {
-            if (Windows.ApplicationModel.DesignMode.DesignModeEnabled)
-            {
-            }
-        }
-
-        
-        public override async Task OnNavigatedToAsync(object parameter, NavigationMode mode, IDictionary<string, object> suspensionState)
-        {
-            if (suspensionState.Any())
-            {
-//                Value = suspensionState[nameof(Value)]?.ToString();
-            }
-           
-            await Task.CompletedTask;
-        }
+        public User User { get; } = new User();
 
         public override async Task OnNavigatedFromAsync(IDictionary<string, object> suspensionState, bool suspending)
         {
             if (suspending)
             {
-                //suspensionState[nameof(Value)] = Value;
+                suspensionState[nameof(User)] = User.Name;
             }
             await Task.CompletedTask;
         }
 
-        public override async Task OnNavigatingFromAsync(NavigatingEventArgs args)
+        public override async Task OnNavigatedToAsync(object parameter, NavigationMode mode, IDictionary<string, object> suspensionState)
         {
-            args.Cancel = false;
+            if (suspensionState.Any())
+            {
+                User.Name = suspensionState[nameof(User)]?.ToString();
+            }
             await Task.CompletedTask;
         }
-        
-        public void GotoJoinSession() =>
-            NavigationService.Navigate(typeof(Views.List), "");
 
-        public void GotoCreateSession() =>
-            NavigationService.Navigate(typeof(Views.NewSession), "");
+        public async void GotoJoinSession()
+        {
+            if (await IsValid())
+            {
+                NavigationService.Navigate(typeof(Views.List), "");
+            }
+        }
+
+        public async void GotoCreateSession()
+        {
+            if (await IsValid())
+            {
+                NavigationService.Navigate(typeof(Views.NewSession), "");
+            }
+        }
+
+        private async Task<bool> IsValid()
+        {
+            if (!string.IsNullOrEmpty(User.Name)) return true;
+            await new MessageDialog("Please enter your name first.").ShowAsync();
+            return false;
+        }
 
         public void GotoAbout() =>
             NavigationService.Navigate(typeof(Views.SettingsPage), 2);
     }
-
 }
-
