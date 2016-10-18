cb67f2a38f90c8f37dca70bbc1779bbc64f3272b
Erik Jan de Wit <erikjan.dewit@gmail.com>
2016-10-18 09:08:11 +0200

    removed unused overrides

--- PointingHenry10/ViewModels/DetailSessionViewModel.cs
+++ PointingHenry10/ViewModels/DetailSessionViewModel.cs
@@ -1,19 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using Template10.Mvvm;
-using Template10.Services.NavigationService;
 using Windows.UI.Xaml.Navigation;
 using PointingHenry10.Models;
-using System.ComponentModel;
-using FHSDK;
-using FHSDK.Config;
-using Quobject.SocketIoClientDotNet.Client;
-using Newtonsoft.Json;
 using System.Collections.ObjectModel;
-using PointingHenry10.Services;
 using PointingHenry10.Services.PokerServices;
 
 namespace PointingHenry10.ViewModels
@@ -22,15 +12,10 @@ namespace PointingHenry10.ViewModels
     {
         public Session SelectedSession;
         public ObservableCollection<User> Users { get; } = new ObservableCollection<User>();
-        public User LoggedUser;
+        private User _loggedUser;
 
         public DetailSessionViewModel()
         {
-            if (Windows.ApplicationModel.DesignMode.DesignModeEnabled)
-            {
-            }
-            SelectedSession = new Session();
-
             PokerService.Instance.SessionUpdatedEvent += (sender, args) =>
                 Dispatcher.Dispatch(() =>
                 {
@@ -42,12 +27,9 @@ namespace PointingHenry10.ViewModels
         public override async Task OnNavigatedToAsync(object parameter, NavigationMode mode,
             IDictionary<string, object> suspensionState)
         {
-            if (suspensionState.Any())
-            {
-            }
             var dict = parameter as Dictionary<string, object>;
-            var session = dict["session"] as Session;
-            var user = dict["user"] as User;
+            var session = dict?["session"] as Session;
+            var user = dict?["user"] as User;
             if (session != null)
             {
                 SelectedSession = session;
@@ -56,23 +38,8 @@ namespace PointingHenry10.ViewModels
             }
             if (user != null)
             {
-                LoggedUser = user;
+                _loggedUser = user;
             }
-            await Task.CompletedTask;
-        }
-
-        public override async Task OnNavigatedFromAsync(IDictionary<string, object> suspensionState, bool suspending)
-        {
-            if (suspending)
-            {
-                //suspensionState[nameof(Value)] = Value;
-            }
-            await Task.CompletedTask;
-        }
-
-        public override async Task OnNavigatingFromAsync(NavigatingEventArgs args)
-        {
-            args.Cancel = false;
             await Task.CompletedTask;
         }
 
